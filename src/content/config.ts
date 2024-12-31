d4bb584019d8fad00f12bed46a43bbf7aad55001
eyemono-moe <ysdr83@gmail.com>
2025-01-01 04:22:55 +0900

    chore: format

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,14 +1,14 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
 const postCollection = defineCollection({
-  type: "content",
-  schema: z.object({
-    title: z.string().nonempty(),
-    tags: z.array(z.string()).optional(),
-    draft: z.boolean().optional(),
-  })
+	type: "content",
+	schema: z.object({
+		title: z.string().nonempty(),
+		tags: z.array(z.string()).optional(),
+		draft: z.boolean().optional(),
+	}),
 });
 
 export const collections = {
-  'posts': postCollection,
+	posts: postCollection,
 };
