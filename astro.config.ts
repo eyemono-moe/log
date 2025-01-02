7739824c8b44e44c286f41484ec6ddc316d19486
eyemono-moe <ysdr83@gmail.com>
2025-01-02 12:37:36 +0900

    feat: add heading links

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,13 @@
 import partytown from "@astrojs/partytown";
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
-import rehypeExternalLinks, { type Options } from "rehype-external-links";
+import rehypeAutolinkHeadings, {
+	type Options as AutoLinkOptions,
+} from "rehype-autolink-headings";
+import rehypeExternalLinks, {
+	type Options as ExternalLinkOptions,
+} from "rehype-external-links";
+import rehypeSlug from "rehype-slug";
 import UnoCSS from "unocss/astro";
 import { remarkModifiedTime } from "./src/plugins/remark-modified-time";
 
@@ -25,6 +31,23 @@ export default defineConfig({
 	markdown: {
 		remarkPlugins: [remarkModifiedTime],
 		rehypePlugins: [
+			rehypeSlug,
+			[
+				rehypeAutolinkHeadings,
+				{
+					behavior: "append",
+					content: {
+						type: "element",
+						tagName: "span",
+						properties: {
+							// @unocss-include
+							class:
+								"inline-block h-1.6rem vertical-top translate-y-[calc((1lh-1.6rem)/2)] w-auto aspect-square i-material-symbols:link-rounded c-[--un-prose-lists] hover:c-accent-7 dark:hover:c-accent transition-color",
+						},
+						children: [],
+					},
+				} satisfies AutoLinkOptions,
+			],
 			[
 				rehypeExternalLinks,
 				{
@@ -38,7 +61,7 @@ export default defineConfig({
 						},
 						children: [],
 					},
-				} satisfies Options,
+				} satisfies ExternalLinkOptions,
 			],
 		],
 	},
