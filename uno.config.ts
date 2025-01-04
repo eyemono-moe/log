1f747797a5cec1e3027813834798398a16988404
eyemono-moe <ysdr83@gmail.com>
2025-01-04 20:47:51 +0900

    refactor: remove Google Fonts

--- uno.config.ts
+++ uno.config.ts
@@ -64,9 +64,6 @@ export default defineConfig({
 	],
 	theme: {
 		colors,
-		fontFamily: {
-			sans: '"Noto Sans JP", ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont,    "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif,    "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";',
-		},
 	},
 	preflights: [
 		{
